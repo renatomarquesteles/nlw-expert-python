d325a52a4e494cfcce664383b6bbcc3d529b4be9
Renato <renatomarquesteles@gmail.com>
2024-02-06 11:50:24 -0300

    chore: adds View for tag creation with typing

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, request, jsonify
+from src.views.http_types.http_request import HttpRequest
+from src.views.tag_creator_view import TagCreatorView
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=['POST'])
 
 def create_tag():
-    print(request.json)
-    return jsonify({ "response": "ok" }), 200
+    tag_creator_view = TagCreatorView()
+    http_request = HttpRequest(body=request.json)
+    http_response = tag_creator_view.validate_and_create(http_request)
+    return jsonify(http_response.body), http_response.status_code
